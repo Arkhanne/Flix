608305a996821724dec31176a5149013f79c8f12
Arkhanne <eduardbenito@icloud.com>
2018-11-22 15:41:21 +0100

    Validations: Part II

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,8 +18,11 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-    redirect_to movie_path
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit
+    end
   end
 
   def new
@@ -28,8 +31,11 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to @movie
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new
+    end
   end
 
   def destroy
